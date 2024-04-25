42988a5052116222d4ea370a47308c5d54baa20a
gustso <gustso@gustsoNT>
2024-04-25 14:58:07 -0300

    Agregada la clase Producto

--- src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,38 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private double precioU;
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public double getPrecioU() {
+		return precioU;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public void setPrecioU(double precioU) {
+		this.precioU = precioU;
+	}
+	
+	
 
 }
