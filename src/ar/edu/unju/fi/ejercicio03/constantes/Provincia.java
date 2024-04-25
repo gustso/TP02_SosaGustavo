b785055086322753bd0966608f2ed3cae647b95d
gustso <gustso@gustsoNT>
2024-04-25 16:20:17 -0300

    finalizado ejercicio03

--- src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
+++ src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
@@ -1,5 +1,46 @@
 package ar.edu.unju.fi.ejercicio03.constantes;
 
 public enum Provincia {
+	
+	JUJUY(673973, 37623),
+    SALTA(1453269, 159549),
+    TUCUMAN(1668312, 25736),
+    CATAMARCA(367828, 102602),
+    LA_RIOJA(389635, 92306),
+    SANTIAGO_DEL_ESTERO(874061, 142587);
 
+	private int cantidadPoblacion;
+	private int superficie;
+	
+	//default
+	private Provincia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//parametrizado
+	private Provincia(int cantidadPoblacion, int superficie) {
+		this.cantidadPoblacion = cantidadPoblacion;
+		this.superficie = superficie;
+	}
+
+	public int getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+
+	public void setCantidadPoblacion(int cantidadPoblacion) {
+		this.cantidadPoblacion = cantidadPoblacion;
+	}
+
+	public double getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
+	
+	public double calcularDensidadPoblacional() {
+        return (double) cantidadPoblacion / superficie;
+    }	
+	
 }
--- src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,10 +1,32 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import java.util.Arrays;
+
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println(Provincia.CATAMARCA.getCantidadPoblacion());	
+		System.out.println(Provincia.JUJUY.getSuperficie());
+		System.out.println(Provincia.SALTA.calcularDensidadPoblacional());
+		
+		Provincia.CATAMARCA.setCantidadPoblacion(200);
+		System.out.println(Provincia.CATAMARCA.getCantidadPoblacion());
+		
+		//mostrar los datos
+		System.out.println(Arrays.toString(Provincia.values()));
+		//List<Provincia> listado = new ArrayList<Provincia>();
+		
+		Provincia[] listado = Provincia.values();
+		
+		for(Provincia p : listado ) {
+			System.out.println(p.getSuperficie());
+		}
 	}
 
 }
